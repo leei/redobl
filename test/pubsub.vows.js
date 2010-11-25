c82fd141206ac59d25617c9e6ec69415dec9b188
Lee Iverson <leei@ece.ubc.ca>
2010-11-25 12:08:33 -0800

    Add basic pubsub commands.

---
+++ test/pubsub.vows.js
@@ -0,0 +1,53 @@
+var vows = require('vows'), assert = require('assert'), sys = require('sys');
+var redis = require('redis-node');
+
+require('underscore');
+var Redobl = require('../lib/redobl').Redobl;
+
+//Redobl.logging = true;
+
+var client = Redobl.defaults.client = redis.createClient();
+client.select(6);
+//sys.log("Default redis = " + sys.inspect(client));
+client.flushdb();
+
+var suite = vows.describe('redobl pub/sub');
+
+suite.addBatch({
+  'a Redobl object': {
+    topic: function () {
+      var Test = Redobl.define("Test");
+      return new Test();
+    },
+
+    'can publish w/o subscribers': {
+      topic: function(test) {
+        test.publish({a: 2}, this.callback);
+      },
+
+      'should return zero': function(err, num) {
+        assert.equal(num, 0);
+      }
+    }
+  }
+});
+
+suite.addBatch({
+  'a Redobl object': {
+    topic: function () {
+      var Test = Redobl.define("Test");
+      return new Test();
+    },
+
+    'can be subscribed to': {
+      topic: function(test) {
+        test.subscribe(this.callback);
+        setTimeout(function() {
+          test.publish({a: 1, b:2});
+        }, 500);
+      }
+    }
+  }
+});
+
+suite.export(module);
